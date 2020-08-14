b5d6bd56ea85cd1cd6683c69a75e80dba5e150fe
khalidhmd <khalidhmd@outlook.com>
2020-08-14 17:30:57 +0200

    a commit

--- src/shared/components/CustomDrawerContent.js
+++ src/shared/components/CustomDrawerContent.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {Image} from 'react-native';
 import {
   DrawerContentScrollView,
   DrawerItemList,
@@ -8,6 +9,15 @@ import {
 export default function CustomDrawerContent(props) {
   return (
     <DrawerContentScrollView {...props}>
+      <DrawerItem
+        label={() => (
+          <Image
+            style={{width: 250, height: 140}}
+            source={require('../../../assets/car2.jpeg')}
+          />
+        )}
+        onPress={() => props.navigation.navigate('UserForm')}
+      />
       <DrawerItemList {...props} />
       <DrawerItem
         label="Help"
