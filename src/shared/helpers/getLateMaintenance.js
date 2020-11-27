c5724a44d9eab0574d6702f3aadd87fb4339b73e
khalidhmd <khalidhmd@outlook.com>
2020-11-27 13:38:38 +0200

    update maintenance reminder 7 days and 200km before due

--- src/shared/helpers/getLateMaintenance.js
+++ src/shared/helpers/getLateMaintenance.js
@@ -3,7 +3,7 @@ export default function(maintenances, car) {
   for (id in maintenances) {
     if (
       maintenances[id].kmRate > 0 &&
-      maintenances[id].kmRate + maintenances[id].km <= car.km
+      maintenances[id].kmRate + maintenances[id].km <= car.km + 200
     ) {
       n += 1;
       continue;
@@ -12,7 +12,11 @@ export default function(maintenances, car) {
     const today = new Date();
     const diffTime = Math.abs(today - maintenanceDate);
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    if (diffDays > maintenances[id].timeRate * 30) n += 1;
+    if (
+      maintenances[id].timeRate > 0 &&
+      diffDays + 7 > maintenances[id].timeRate * 30
+    )
+      n += 1;
   }
   return n;
 }
